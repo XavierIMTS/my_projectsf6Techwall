6d36d742330fdaf85391e99b962451b4a3469637
XavierIMTS <114238634+XavierIMTS@users.noreply.github.com>
2023-01-10 15:36:04 +0100

    Récupérer  paramètres route dans un controlleur

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,17 +19,13 @@ class FirstController extends AbstractController
         ]);
     }
 
-    #[Route('/sayhello', name: 'sayhello')]
-    public function sayHello(): Response
+    #[Route('/sayhello/{name}/{firstname}', name: 'say.hello')]
+    public function sayHello(Request $request, $name, $firstname): Response
     {
-        if(rand(0,10) % 2){
-            //return $this->redirectToRoute('FirstController');
-            return $this->forward('App\Controller\FirstController::index');
-        }
-        return $this->render('first/hello.html.twig', [
-           'controller_name' => 'SayHello',
-           'firstname' => 'Robert',
-           'lastname' => 'DURANT',
+        dd($request);
+        return $this->render('first/hello.html.twig', [ 
+            'name' => $name,
+            'firstname' => $firstname,
         ]);
 
 
